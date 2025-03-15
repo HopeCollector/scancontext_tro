5f136253f0d2468ef9cee71f503c0b7925d7a9f7
HopeCollector <cmw0249@gmail.com>
2025-03-15 08:26:37 +0000

    feat(expr): Add initial CMake configuration and project files

---
+++ expr/main.cc
@@ -0,0 +1,10 @@
+#include "Scancontext.h"
+#include "expr-utils/data_loader.hh"
+#include <fmt/format.h>
+#include <CLI/App.hpp>
+#include <fkYAML/node.hpp>
+
+int main(int argc, char** argv) {
+  fmt::print("hi");
+  return 0;
+}
