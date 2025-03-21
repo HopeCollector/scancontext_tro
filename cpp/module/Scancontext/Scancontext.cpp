b1c767e433be819b0e7eba83393b3fd2a06796ea
HopeCollector <cmw0249@gmail.com>
2025-03-21 14:22:52 +0000

    fix(scancontext): Optimize loop closure detection by adjusting results handling

--- cpp/module/Scancontext/Scancontext.cpp
+++ cpp/module/Scancontext/Scancontext.cpp
@@ -292,8 +292,6 @@ std::pair<int, float> SCManager::detectLoopClosureID ( void )
     /* 
      *  step 2: pairwise distance (find optimal columnwise best-fit using cosine distance)
      */
-    results.clear();
-    results.reserve( NUM_CANDIDATES_FROM_TREE );
     TicToc t_calc_dist;   
     for ( int candidate_iter_idx = 0; candidate_iter_idx < NUM_CANDIDATES_FROM_TREE; candidate_iter_idx++ )
     {
@@ -310,10 +308,10 @@ std::pair<int, float> SCManager::detectLoopClosureID ( void )
 
             nn_idx = candidate_indexes[candidate_iter_idx];
         }
-
-        results.emplace_back(candidate_indexes[candidate_iter_idx], candidate_dist);
     }
     t_calc_dist.toc("Distance calc");
+    results.clear();
+    results.emplace_back( nn_idx, min_dist );
 
     /* 
      * loop threshold check
